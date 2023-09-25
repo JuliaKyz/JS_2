121a69b260e04344ad8b5930daf2bac113a5c4ed
JuliaKyz <Julie.abukhova@gmail.com>
2023-09-25 23:15:38 +0300

    added lesson_8

---
+++ lesson_8_1/public/js/FooterComponent.js
@@ -0,0 +1,26 @@
+const footer = {
+  template: `
+  <div class="footer">
+  <div class="container">
+    <div class="footerLeft">
+      <a href="#">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="#">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="#">
+        <i class="fab fa-pinterest-p"></i>
+      </a>
+      <a href="#">
+        <i class="fab fa-twitter"></i>
+      </a>
+    </div>
+    <div class="footerRight">&copy; 2021 Brand All Rights Reserved.</div>
+  </div>
+</div>
+  `,
+};
+export default footer;
+
+
